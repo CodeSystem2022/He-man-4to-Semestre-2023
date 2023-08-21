566db9e19b801853b42e1b14772664ed790dfe75
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-08-21 16:49:58 -0300

    1.2 Creamos el main -> SistemaEstudiantesApp.java
    1.2 Creamos el main -> SistemaEstudiantesApp.java

---
+++ Java/Leccion1/SistemasEstudiantes/src/main/java/UTN/presentacion/SistemaEstuiantesApp.java
@@ -0,0 +1,25 @@
+package UTN.presentacion;
+
+import UTN.conexion.Conexion;
+import UTN.datos.EstudianteDAO;
+import UTN.dominio.Estudiante;
+
+import java.util.Scanner;
+
+public class SistemaEstudianesApp {
+    public static void main(String[] args) {
+        var salir = false; //recuerden esto ya lo hicimos antes
+        var consola = new Scanner(System.in); //Para leer informacion de la consola
+        //Se crea una instancia de la clase servicio, esto lo hacemos fuera del ciclo
+        var estudianteDao = new EstudianteDAO();//Esta instancia debe hacerse una vez
+        while(!salir){
+            try{
+                mostrarMenu();//Mostramos el menu
+                //Este será el metodo ejecutarOpciones que devolvera un booleano
+                salir = ejecutarOpciones(consola, estudianteDao);//Este arroja una excetion
+            } catch(Exception e){
+                System.out.println("Ocurrio un error al ejecutar la operacion:" +e.getMessage());
+            }
+        }//Fin while
+    }//Fin main
+}//Fin clase
