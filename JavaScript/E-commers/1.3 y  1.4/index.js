709f0c6f1748a77d36629303b69b9b66f59cbb01
Mauricio Cerda <112664533+MauryCerda@users.noreply.github.com>
2023-08-25 16:55:58 -0300

    1.3, 1.4 - cerda
    1.3 Avanzamos con css y js ,  1.4 Mostramos detalles finales

---
+++ JavaScript/E-commers/1.3 y  1.4/index.js
@@ -0,0 +1,12 @@
+const shopContent = dodument.getElementById("shopContent");
+
+productos.forEach((product) =>{
+	const content = document.createElement("div");
+	content.innerHTML = ´
+	<img src="${product.img}">
+	<h3>${product.productName}</h3>
+	<p>${product.price}</p>
+	´;
+	shopContent.append(content);
+
+});
