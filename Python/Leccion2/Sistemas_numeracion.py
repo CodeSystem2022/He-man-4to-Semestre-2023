b2c19173f85627d6a34fc0aef35dbec238c6e901
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-09-11 17:16:32 -0300

    Sistemas_numeracion
    Sistemas_numeracion

---
+++ Python/Leccion2/Sistemas_numeracion.py
@@ -0,0 +1,26 @@
+#Profundizando cen los sistemas de numeracion
+
+#Sistema decimal
+a = 10
+print(f'a decimal = {a}')
+
+#Sistema binario
+a = 0b1010
+print(f'a binario= {a}')
+
+#sistema octal
+a = 0o12
+print(f'a octal = {a}')
+
+#sistema hexadecimal
+a = 0xA
+print(f'a hexadecimal = {a}')
+print("******************")
+
+#base decimal base 10
+a = int('23',10)
+print(f'a base decimal = {a}')
+
+#base binario   base 2
+a= int('10111',2)
+print(f'a base binario= {a}')
