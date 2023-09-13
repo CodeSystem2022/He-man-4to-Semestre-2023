57197257931456aebd790a4a73cd18ba28c8a32a
fabjav <112377555+fabjav@users.noreply.github.com>
2023-09-13 18:27:37 -0300

    2.3 parte 1 lista

--- JavaScript/E-commers/client/js/cart.js
+++ JavaScript/E-commers/client/js/cart.js
@@ -1,3 +1,13 @@
 const modalContainer = document.getElementById("modal-container");
 const modalOverlay = document.getElementById("modal-overlay");
 
+
+const displayCart = () => {
+    //modal Header
+    const modalHeader = document.createElement("div");
+
+    const modalClose = document.createElement("vid");
+    modalClose.innerText = "❌"
+    modalClose.className = "modal-close";
+    modalHeader.append(modalClose)
+}
