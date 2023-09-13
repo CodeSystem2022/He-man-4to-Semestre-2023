e40859379a9a48a619274ad9a115b953bc1c5d14
fabjav <112377555+fabjav@users.noreply.github.com>
2023-09-13 18:30:02 -0300

    title

--- JavaScript/E-commers/client/js/cart.js
+++ JavaScript/E-commers/client/js/cart.js
@@ -10,4 +10,9 @@ const displayCart = () => {
     modalClose.innerText = "❌"
     modalClose.className = "modal-close";
     modalHeader.append(modalClose)
+
+    const modalTitle = document.createElement("div");
+    modalTitle.innerText = "cart";
+    modalTitle-className = "modal-title";
+    modalHeader.append("modalTitle");
 }
