e90fd31317d445b4025b6be36989fed63eaca442
Fernando Cala <fernando221298@gmail.com>
2023-09-13 21:56:14 -0300

    3.2 Utilizamos el setTimeOut y hacemos pruebas en ejecución
    Subimos ultima parte del codigo de la clase

---
+++ node/01-HolaMundo/app3.js
@@ -0,0 +1,18 @@
+console.log("Inicio del programa"); //1
+
+setTimeout(()=>{
+    console.log("Primer Timeout"); //5
+}, 3000);
+
+setTimeout(()=>{
+    console.log("Segundo Timeout"); //3
+}, 0);
+
+setTimeout(()=>{
+    console.log("Tercero Timeout"); //4
+}, 0);
+
+console.log("Fin del programa"); //2
+
+//Cae en un stack de procedimientos y tiene que ejecutar.
+//Todo es sincrono, son instrucciones no bloqueantes.
