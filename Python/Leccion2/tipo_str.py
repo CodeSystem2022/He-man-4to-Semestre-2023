84557c1427c853d1f008346b5fc4366983b2e034
Jorge-Agustin-Loyola <112104103+Jorge-Agustin-Loyola@users.noreply.github.com>
2023-09-19 00:26:20 -0300

    python 3.3/3.4.1/3.4.2

---
+++ Python/Leccion2/tipo_str.py
@@ -0,0 +1,13 @@
+# Profundizando en el tipo String
+# Concatenacion automática en Python
+import math
+
+variable = " Adíos"
+mensaje = 'Hola '"Alumnos" + variable
+
+mensaje += ", Terminamos"
+print(mensaje)
+
+# Usamos la clse help para ayuda o documentacion (built-in)
+help(str)
+help(math)
