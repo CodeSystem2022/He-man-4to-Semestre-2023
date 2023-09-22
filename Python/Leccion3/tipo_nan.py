106d96e946ca98651bb04579c2af064e943cd117
fabjav <112377555+fabjav@users.noreply.github.com>
2023-09-22 19:02:03 -0300

    3.1 L4 terminada py

---
+++ Python/Leccion3/tipo_nan.py
@@ -0,0 +1,14 @@
+
+# NaN (Not a Number)
+import math
+
+a = float('nan')
+print(f'a: {a}')
+
+#Modulo Math
+a= float('nan')
+print(f'Es de tipo NaN (Not a Number)?: {math.isnan(a)}')
+
+#Modulo decimal
+a = Decimal('nan')
+print(f'Es de tipo NaN(Not a Number)?: {math.isnan(a)}')
