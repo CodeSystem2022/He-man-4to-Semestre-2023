9f85ec1a40d55cd4196eaf21ac93fa24accd0bd9
Fernando Cala <fernando221298@gmail.com>
2023-10-02 09:49:53 -0300

    5.4 Ejecutamos para aclarar conceptos
    Subo codigo NODE

--- node/01-HolaMundo/monohilo.js
+++ node/01-HolaMundo/monohilo.js
@@ -6,10 +6,12 @@ setInterval(function() {
     console.log(i);
     i++;
 
-    if (i===5){
-        console.log("Forzamos un error");
-        var a = 3 + z;
-
-    }
+//if (i===5){
+//    console.log("Forzamos un error");
+//    var a = 3 + z;
+// }
 
 },1000);
+
+console.log("Segunda instruccion");
+
