219fe1e0f96ba73c345e12f0b3b811a7e90e1681
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-10-03 17:45:14 -0300

    5.4Metodo ejecutarOpciones-ModificarEstudiante
    Parte 3

--- Java/Leccion4/EstudiantesApplication.java
+++ Java/Leccion4/EstudiantesApplication.java
@@ -93,6 +93,33 @@ public class EstudiantesApplication implements CommandLineRunner {
 				estudianteServicio.guardarEstudiante(estudiante);
 				logger.info("Estudiante agregado: "+estudiante+nl);
 			}
+			case 4 -> { //Modificar estudiante
+				logger.info("Modificar estudiante: "+nl);
+				logger.info("Ingrese el id estudiante: ");
+				var idEstudiante = Integer.parseInt(consola.nextLine());
+				// Buscamos el estudiante a modificar
+				Estudiantes2022 estudiante =
+						estudianteServicio.buscarEstudiantePorId(idEstudiante);
+				if(estudiante != null){
+					logger.info("Nombre: ");
+					var nombre = consola.nextLine();
+					logger.info("Apellido: ");
+					var apellido = consola.nextLine();
+					logger.info("Telefono: ");
+					var telefono = consola.nextLine();
+					logger.info("Email: ");
+					var email = consola.nextLine();
+					estudiante.setNombre(nombre);
+					estudiante.setApellido(apellido);
+					estudiante.setTelefono(telefono);
+					estudiante.setEmail(email);
+					estudianteServicio.guardarEstudiante(estudiante);
+					logger.info("Estudiante modificado: "+estudiante+nl);
+				}
+				else
+					logger.info("Estudiante NO encontrado con el id: "+ idEstudiante+nl);
+
+			}
 			
 		}//Fin switch
 		return salir;
