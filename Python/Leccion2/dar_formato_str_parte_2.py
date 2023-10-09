81750bd2e5983d238bd8d29259f1a934806ad78b
Mauricio Cerda <112664533+MauryCerda@users.noreply.github.com>
2023-10-09 17:13:46 -0300

    dar_formato_str_parte_2
    dar_formato_str_parte_2

---
+++ Python/Leccion2/dar_formato_str_parte_2.py
@@ -0,0 +1,10 @@
+# dar formato a un string
+
+nombre = 'Ariel'
+edad = 28
+mensaje_con_formato = 'Mi nombre es %s y tengo %d años' % (nombre, edad)
+
+# Creamos una tupla 
+persona = ('carla', 'gomez', 5000.00)
+mensaje_con_formato = 'Hola %s %s . Tu sueldo es %.2f'  # % persona # Aqui le pasamos el objeto que es tupla
+print(mensaje_con_formato % persona)
