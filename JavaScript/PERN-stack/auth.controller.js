eae837f9d5e8b058561ba9f452acaed864410f70
Mauricio Cerda <112664533+MauryCerda@users.noreply.github.com>
2023-10-19 17:45:25 -0300

    Utilización de bcrypt
    Utilización de bcrypt para proteger el password Instalar -> npm i bcrypt

---
+++ JavaScript/PERN-stack/auth.controller.js
@@ -0,0 +1,21 @@
+import {pool} from "../db.js"
+import bcrypt from "bcrypt";
+
+export const signin = (req, res)=> res.send("ingresando");
+
+export const signup = async(req, res)=>{
+	const {name, email, password}= req.body;
+
+	try{
+		const hashedPassword = await bcrypt.hash(password, 10);
+		console.log(hashedPassword)
+
+		const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword]);
+		console.log(result);
+		return res.json(result.rows[0]);
+	}catch (error){
+		if(error.code ==="23505"){
+			return res.status(400).json({message: "El correo ya esta registrado"});
+		}
+	}
+};
