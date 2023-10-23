08362739ae621a85ec8ef91188a7c6c430857de6
fabjav <112377555+fabjav@users.noreply.github.com>
2023-10-23 18:33:28 -0300

    importacion de token en auth.controller

--- JavaScript/PERN-stack/auth.controller.js
+++ JavaScript/PERN-stack/auth.controller.js
@@ -1,6 +1,6 @@
 import {pool} from "../db.js"
 import bcrypt from "bcrypt";
-
+import {createAcccesToken} from "../libs/jwt.js"
 export const signin = (req, res)=> res.send("ingresando");
 
 export const signup = async(req, res)=>{
@@ -11,8 +11,13 @@ export const signup = async(req, res)=>{
 		console.log(hashedPassword)
 
 		const result = await pool.query("INSERT INTO usuarios (name, email, password) VALUES ($1, $2, $3) Returning *", [name, email, hashedPassword]);
+
+		const token = await createAcccesToken({id: result.rows[0].id});
 		console.log(result);
-		return res.json(result.rows[0]);
+		//return res.json(result.rows[0]);
+		return res.json({
+			token: token,
+		});
 	}catch (error){
 		if(error.code ==="23505"){
 			return res.status(400).json({message: "El correo ya esta registrado"});
