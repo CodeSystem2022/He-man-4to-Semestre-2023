a9c5da0c93569f8372b76ae85568b5bb4b6842c9
fabjav <112377555+fabjav@users.noreply.github.com>
2023-10-23 18:40:25 -0300

    corrección

--- JavaScript/PERN-stack/auth.controller.js
+++ JavaScript/PERN-stack/auth.controller.js
@@ -14,17 +14,15 @@ export const signup = async(req, res)=>{
 
 		const token = await createAcccesToken({id: result.rows[0].id});
 		console.log(result);
-		res.cookie("token", token {
+		res.cookie("token", token, {
 			httpOnly: true,
 			secure: false,
 			sameSite: "none",
-			maxAge: 60 * 60 * 24 * 1000, }) // 1 day
+			maxAge: 60 * 60 * 24 * 1000,}) // 1 day
 
 		
-		//return res.json(result.rows[0]);
-		return res.json({
-			token: token,
-		});
+		return res.json(result.rows[0]);
+		
 	}catch (error){
 		if(error.code ==="23505"){
 			return res.status(400).json({message: "El correo ya esta registrado"});
