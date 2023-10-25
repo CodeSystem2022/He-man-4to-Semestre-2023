7bfa18abc92f164818273e0e515180e95883e3d1
1juba <112666824+1juba@users.noreply.github.com>
2023-10-24 22:38:42 -0300

    8.2-Terminando con el main
    8.2-Terminando con el main

--- Java/Leccion5 - Tienda de libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
+++ Java/Leccion5 - Tienda de libros/tienda_libros/src/main/java/utn/tienda_libros/TiendaLibrosApplication.java
@@ -1,13 +1,27 @@
 package utn.tienda_libros;
-//l
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import utn.tienda_libros.vista.LibroFrom;
+import java.awt.EventQueue;
 @SpringBootApplication
 public class TiendaLibrosApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TiendaLibrosApplication.class, args);
-	}
+		ConfigurableApplicationContext contextoSpring =
+			new SpringApplication.run(TiendaLibrosApplication.class)
+					.headless(false)
+					.web(WebApplicationType.NONE)
+					.run(args);
 
+		//Ejecutamos el codigo para cargar el formulario
+		EventQueue.invokeLater(() -> {        //metodo Lambda
+		BeanFactory contextoSpring;
+			//Obtenemos el objeto form a traves del Spring
+		LibroFrom libroFrom = contextoSpring.getBean(LibroFrom.class);
+		libroFrom.setVisible(true);
+				});
+		}
 }
