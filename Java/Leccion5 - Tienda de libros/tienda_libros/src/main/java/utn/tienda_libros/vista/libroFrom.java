68803b060cdf10eac0d77245e2fe4e13bcf85b34
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-10-25 16:42:54 -0300

    .3 Avanzamos con swing y la tabla -> JTable Parte 2

--- Java/Leccion5 - Tienda de libros/tienda_libros/src/main/java/utn/tienda_libros/vista/libroFrom.java
+++ Java/Leccion5 - Tienda de libros/tienda_libros/src/main/java/utn/tienda_libros/vista/libroFrom.java
@@ -19,14 +19,22 @@ public class LibroFrom extends JFrame {
         iniciarForma();
 
 private void iniciarForma() {
-        setContentPane(panel);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
-        setSize(900, 700);
-        //  Para obtener las dimensiones de la ventana
-        Toolkit toolkit = Toolkit.getDefaultToolkit();
-        Dimension tamanioPantalla = toolkit.getScreenSize();
-        int x = (tamanioPantalla.width - getWidth() / 2);
-        int y = (tamanioPantalla.height - getHeight() / 2);
-        setLocation(x, y);
+    setContentPane(panel);
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);
+    setSize(900, 700);
+    //  Para obtener las dimensiones de la ventana
+    Toolkit toolkit = Toolkit.getDefaultToolkit();
+    Dimension tamanioPantalla = toolkit.getScreenSize();
+    int x = (tamanioPantalla.width - getWidth() / 2);
+    int y = (tamanioPantalla.height - getHeight() / 2);
+    setLocation(x, y);
+}
+
+private void createUIComponents() {
+    this.tablaModeloLibros =new DefaultTableModel(0 ,5)
+    String[] cabecera = {"Id", "Libro", "Autor", "Precio", "Existencia"};
+    this.tablaModeloLibros.setColumnIdentifiers(cabecera);
+    // Instanciar el objeto de JTable
+    this.tablaLibros = new JTable(tablaModeloLibros);
 }
