0beb22887adc4a91af05d6266026c346b0264a69
fabjav <112377555+fabjav@users.noreply.github.com>
2023-10-26 20:42:00 -0300

    8.1 Comencemos creando carpeta, archivo y código

---
+++ node/02-Fundamentos_node/async/callback.js
@@ -0,0 +1,7 @@
+function soyAsincrona(){
+    console.log('Hola, soy una funcion asincrona');
+
+}
+console.log('Iniciando el proceso...');
+soyAsincrona();
+console.log('Terminando el proceso...');
