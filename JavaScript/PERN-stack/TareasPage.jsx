0d49aba39fe6926bf2004c01c1118fe7e91a6260
Mauricio Cerda <112664533+MauryCerda@users.noreply.github.com>
2023-11-02 18:25:07 -0300

    Add files via upload

--- JavaScript/PERN-stack/TareasPage.jsx
+++ JavaScript/PERN-stack/TareasPage.jsx
@@ -10,8 +10,16 @@ function TareasPage(){
 		cargarTareas();
 	}, []);
 
+	if(tarea.length === 0){
+		return(
+			<div className="flex justify-center items-center h-[calc(100vh-10rem}]">
+				<h1 className="text-3x1 font-bold"> No hay tareas</h1>
+			</div>
+			)
+	}
+
 	return (
-		<div classNone = "grid grid-cols-3 gap.2">{
+		<div classNone = "grid3md:grid-cols-2 lg:grid-cols-3  gap-2">{
 			tareas.map((tarea)=> (
 				<CardTareas tarea=(tarea) key(tarea.id)/>
 				)
