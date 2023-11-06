c93d94c2372a252313ff787cb317218fb17b8aa9
MatiasMichaux <112663570+MatiasMichaux98@users.noreply.github.com>
2023-11-05 23:18:34 -0300

    10.1 Introducción a las promesas  al final estará el video del canal HolaMundo
    10.1 Introducción a las promesas  al final estará el video del canal HolaMundo

---
+++ Node/02-Fundamentos_node/async/promises.js
@@ -0,0 +1,20 @@
+function hola(nombre, miCallback){
+    setTimeout(function (){
+        console.log("Hola"+nombre);
+        miCallback(nombre);
+    }, 1000);  
+}
+
+function hablar(callbackHablar){
+    setTimeout(function() {
+        console.log("bla bla bla");
+        callbackHablar();
+    }, 1000);
+}
+
+function adios(nombre, otrocallback) {
+    setTimeout(function() {
+        console.log("Adios "+ nombre);
+        otrocallback();
+    }, 1000);
+}
