465b646280ee8198c05a3d1af78c3d7d1fbaa910
EMMANUEL-1985 <112596295+Emmanuel-1985@users.noreply.github.com>
2023-11-09 22:16:49 -0300

    12.1 Introducción a módulos. creamos una carpeta: modulos y un archivo: globales.js
    12.1 Introducción a módulos . creamos una carpeta: modulos y un archivo: globales.js

---
+++ node/02-Fundamentos_node/modulos/globales.js
@@ -0,0 +1,17 @@
+// this === global = true
+
+//Mostrar algo en consola
+console.log();
+
+//Mostrar un mensaje en forma de error
+console.error();
+
+//Ejecutar un codigo despues de un intervalo de tiempo
+setTimeout(() => {});
+
+//Ejecutar un codigo cada intervalo de tiempo
+setInterval(() => {});
+
+//Da prioridad de ejecucion a una funcion asincronica
+setImmdiate(() => {});
+
