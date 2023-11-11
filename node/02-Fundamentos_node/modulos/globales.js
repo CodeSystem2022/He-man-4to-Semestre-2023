ed7ed55a10432df5bb85092b139d57f7aabd9629
1juba <112666824+1juba@users.noreply.github.com>
2023-11-10 21:38:20 -0300

    12.3-Herramientas-modulo-global
    12.3-Herramientas-modulo-global

--- node/02-Fundamentos_node/modulos/globales.js
+++ node/02-Fundamentos_node/modulos/globales.js
@@ -15,5 +15,25 @@ console.error();
 //Da prioridad de ejecucion a una funcion asincronica
 //setImmdiate(() => {});
 
-console.log(setInterval);
+//console.log(setInterval);
+
+let i = 0;
+let intervalo = setInterval(() => {
+    console.log('Hola');
+    if (i ===3){ 
+    clearInterval(intervalo); //detenemos la funcion
+}
+i++;
+}, 1000);
+
+setImmediate(() => {
+    console.log(`Saludo inmediato`);
+});
+
+//require();
+
+console.log(__filename);
+
+global.miVariable = `mi variable global`;
+console.log(miVariable);
 
