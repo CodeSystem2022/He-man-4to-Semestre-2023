98b8769118342724910eae6528b0aa3f997fec25
Sebastian Barros <112724907+2022sbm@users.noreply.github.com>
2023-11-10 23:03:28 -0300

    13.3 Escribir y crear un archivo
    13.3

--- Node/02-Fundamentos_node/modulos/fs.js
+++ Node/02-Fundamentos_node/modulos/fs.js
@@ -1,1 +1,18 @@
 const fs = require('fs');
+//Primeero leemos el archivo.txt
+function leer(ruta, cb){
+    fs.readFile(ruta, (err, data) => {
+        cb(data.toString());
+    })
+}
+//Segundo escribimos el archivo.txt creandolo
+function escribir(ruta, contenido, cb){
+    fs.writeFile(ruta, contenido, function (err){
+        if (err) {
+            console.log("No se ha podido escribir", err);
+        } else {
+            console.log("Se ha escrito correctamente");
+        }
+    })
+}
+
